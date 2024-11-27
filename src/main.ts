e167b932a6e9155045316f61e45570cead674d2f
ArturKojoyan <arturkojoyan15@mail.ru>
2024-11-27 22:07:36 +0400

    fix: add missing controller and service to main Module

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
-    console.log('Server started listening on port 3000');
+  await app.listen(process.env.PORT || 3003, () => {
+    console.log(`Server started listening on port ${process.env.PORT}`);
   });
 }
 bootstrap();
