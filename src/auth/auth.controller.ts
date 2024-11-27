196a6426cce48808d8aee56d3d4540c48fe5aac5
ArturKojoyan <arturkojoyan15@mail.ru>
2024-11-27 22:08:05 +0400

    feat: add userDto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserDto } from './dto/user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() body: { email: string; password: string }) {
+  async register(@Body() body: UserDto) {
     return this.authService.register(body.email, body.password);
   }
 
   @Post('login')
-  async login(@Body() body: { email: string; password: string }) {
+  async login(@Body() body: UserDto) {
     return this.authService.login(body.email, body.password);
   }
 }
