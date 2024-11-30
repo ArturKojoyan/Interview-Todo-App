03b33fcfdb84d6009c877a84798608c550d12668
ArturKojoyan <arturkojoyan15@mail.ru>
2024-11-30 23:31:56 +0400

    feat: add update and delete routes, also updateTodo and deleteTodo services

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get, Post, Body, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Req,
+  UseGuards,
+  Patch,
+  Delete,
+  Param,
+  ParseIntPipe,
+  NotFoundException,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('todos')
@@ -13,8 +26,39 @@ export class TodosController {
     return this.todosService.createTodo(req.user.id, body.title);
   }
 
+  @Get('/:id')
+  async findOne(@Param('id', ParseIntPipe) id, @Req() req) {
+    const todo = await this.todosService.findOneTodo(id, req.user.id);
+    if (!todo) {
+      throw new NotFoundException();
+    }
+    return todo;
+  }
+
   @Get()
   async list(@Req() req) {
     return this.todosService.listTodos(req.user.id);
+  }
+
+  @Patch('/:id')
+  async update(
+    @Param('id', ParseIntPipe) id,
+    @Body() body: UpdateTodoDto,
+    @Req() req,
+  ) {
+    const todo = await this.todosService.findOneTodo(id, req.user.id);
+    if (!todo) {
+      throw new NotFoundException('todo with provided id is not found');
+    }
+    return this.todosService.updateTodo(id, body);
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id', ParseIntPipe) id, @Req() req) {
+    const todo = await this.todosService.findOneTodo(id, req.user.id);
+    if (!todo) {
+      throw new NotFoundException('todo with provided id is not found');
+    }
+    return this.todosService.deleteTodo(id);
   }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Injectable()
 export class TodosService {
   constructor(private readonly prisma: PrismaService) {}
 
   async createTodo(userId: number, title: string) {
-    return this.prisma.todo.create({
+    return await this.prisma.todo.create({
       data: { title, userId },
     });
   }
 
+  async findOneTodo(id: number, userId: number) {
+    return await this.prisma.todo.findFirst({ where: { id, userId } });
+  }
+
   async listTodos(userId: number) {
-    return this.prisma.todo.findMany({ where: { userId } });
+    return await this.prisma.todo.findMany({ where: { userId } });
+  }
+
+  async updateTodo(id: number, payload: UpdateTodoDto) {
+    return await this.prisma.todo.update({
+      where: {
+        id,
+      },
+      data: payload,
+    });
+  }
+
+  async deleteTodo(id: number) {
+    return await this.prisma.todo.delete({ where: { id } });
   }
 }
