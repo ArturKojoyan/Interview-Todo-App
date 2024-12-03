6bb54d7a8207548b20558b03f0563ea6f8a73dd7
ArturKojoyan <arturkojoyan15@mail.ru>
2024-12-03 19:12:18 +0400

    fix: fix todos controller test

--- src/todos/todos.controller.spec.ts
+++ src/todos/todos.controller.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { NotFoundException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
+import { TodosModule } from './todos.module';
 
 describe('TodosController', () => {
   let controller: TodosController;
@@ -18,9 +18,6 @@ describe('TodosController', () => {
       };
     }),
     findOneTodo: jest.fn((id) => {
-      if (id === 10) {
-        return null;
-      }
       return {
         id,
         title: expect.any(String),
@@ -38,12 +35,12 @@ describe('TodosController', () => {
         },
       ];
     }),
-    updateTodo: jest.fn((id, payload) => {
+    updateTodo: jest.fn((id, userId, payload) => {
       return {
         id,
         title: payload.title,
         completed: payload.completed,
-        userId: expect.any(Number),
+        userId,
       };
     }),
     deleteTodo: jest.fn((id) => {
@@ -56,9 +53,16 @@ describe('TodosController', () => {
     }),
   };
 
+  const mockUser = {
+    id: 1,
+    email: expect.any(String),
+    password: expect.any(String),
+  };
+
   beforeEach(async () => {
     // mocking the module
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TodosModule],
       controllers: [TodosController],
       providers: [TodosService],
     })
@@ -80,21 +84,24 @@ describe('TodosController', () => {
     mockTodoService.deleteTodo.mockClear();
   });
 
-  it('todo controller should be defined', () => {
+  it('todo module: should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  it('create: should create new todo', async () => {
-    const req = {
-      user: { id: 1 },
-    };
+  it('todo controller: should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('create handler: should call createTodo service correctly', async () => {
     const dto = {
       id: expect.any(Number),
       title: 'New Todo',
       completed: false,
       userId: 1,
     };
-    expect(await controller.create({ title: dto.title }, req)).toEqual(dto);
+    expect(await controller.create({ title: dto.title }, mockUser)).toEqual(
+      dto,
+    );
 
     expect(mockTodoService.createTodo).toHaveBeenCalledTimes(1);
     expect(mockTodoService.createTodo).toHaveBeenCalledWith(
@@ -103,9 +110,11 @@ describe('TodosController', () => {
     );
   });
 
-  it('list: should get specific user todos', async () => {
-    const req = {
-      user: { id: 1 },
+  it('list handler: should call listTodos service correctly', async () => {
+    const args = {
+      user: mockUser,
+      limit: 5,
+      offset: 0,
     };
     const dto = [
       {
@@ -115,111 +124,70 @@ describe('TodosController', () => {
         userId: 1,
       },
     ];
-    expect(await controller.list(req)).toEqual(dto);
+    expect(await controller.list(args.user, args.limit, args.offset)).toEqual(
+      dto,
+    );
 
     expect(mockTodoService.listTodos).toHaveBeenCalledTimes(1);
-    expect(mockTodoService.listTodos).toHaveBeenCalledWith(1);
+    expect(mockTodoService.listTodos).toHaveBeenCalledWith(
+      args.user.id,
+      args.limit,
+      args.offset,
+    );
   });
 
-  it('findOne: should get todo when id is 1', async () => {
-    const req = {
-      user: { id: 1 },
-    };
+  it('findOne handler: should call findOneTodo service correctly', async () => {
     const dto = {
       id: 1,
       title: expect.any(String),
       completed: expect.any(Boolean),
       userId: 1,
     };
-    expect(await controller.findOne(dto.id, req)).toEqual(dto);
+    expect(await controller.findOne(dto.id, mockUser)).toEqual(dto);
 
     expect(mockTodoService.findOneTodo).toHaveBeenCalledTimes(1);
     expect(mockTodoService.findOneTodo).toHaveBeenCalledWith(1, dto.userId);
   });
 
-  it('findOne: should throw error when todo id is 10', async () => {
-    const req = {
-      user: { id: 1 },
-    };
-    await expect(controller.findOne(10, req)).rejects.toThrow(
-      NotFoundException,
-    );
-
-    expect(mockTodoService.findOneTodo).toHaveBeenCalledTimes(1);
-  });
-
-  it('update: should update todo correctly when id is not 10', async () => {
-    const req = {
-      user: { id: 1 },
-    };
+  it('update handler: should call updateTodo service correctly', async () => {
     const dto = {
       id: 1,
       title: 'new title',
       completed: true,
       userId: expect.any(Number),
     };
-    expect(
-      await controller.update(
-        dto.id,
-        {
-          title: dto.title,
-          completed: dto.completed,
-        },
-        req,
-      ),
-    ).toEqual(dto);
-
-    expect(mockTodoService.findOneTodo).toHaveBeenCalled();
-    expect(mockTodoService.updateTodo).toHaveBeenCalledTimes(1);
-    expect(mockTodoService.updateTodo).toHaveBeenCalledWith(dto.id, {
+    // expect update return value to be equal to result
+    const result = await controller.update(dto.id, mockUser, {
       title: dto.title,
       completed: dto.completed,
     });
-  });
+    expect(result).toEqual(dto);
 
-  it('update: should throw error when todo id is 10', async () => {
-    const req = {
-      user: { id: 1 },
-    };
-    const dto = {
-      id: 10,
-      title: 'new title',
-      completed: true,
-      userId: expect.any(Number),
-    };
-    await expect(controller.update(10, dto, req)).rejects.toThrow(
-      NotFoundException,
+    expect(mockTodoService.updateTodo).toHaveBeenCalledTimes(1);
+    expect(mockTodoService.updateTodo).toHaveBeenCalledWith(
+      dto.id,
+      mockUser.id,
+      {
+        title: dto.title,
+        completed: dto.completed,
+      },
     );
-
-    expect(mockTodoService.findOneTodo).toHaveBeenCalledTimes(1);
-    expect(mockTodoService.updateTodo).toHaveBeenCalledTimes(0);
   });
 
-  it('delete: should delete todo correctly when id is not 10', async () => {
-    const req = {
-      user: { id: 1 },
-    };
+  it('delete handler: should call deleteTodo service correctly', async () => {
     const dto = {
       id: 1,
       title: expect.any(String),
       completed: expect.any(Boolean),
       userId: expect.any(Number),
     };
-    expect(await controller.delete(dto.id, req)).toEqual(dto);
+    // expect delete return value to be equal to result
+    expect(await controller.delete(dto.id, mockUser)).toEqual(dto);
 
-    expect(mockTodoService.findOneTodo).toHaveBeenCalled();
     expect(mockTodoService.deleteTodo).toHaveBeenCalledTimes(1);
-    expect(mockTodoService.deleteTodo).toHaveBeenCalledWith(dto.id);
-  });
-
-  it('delete: should throw error when todo id is 10', async () => {
-    const req = {
-      user: { id: 1 },
-    };
-
-    await expect(controller.delete(10, req)).rejects.toThrow(NotFoundException);
-
-    expect(mockTodoService.findOneTodo).toHaveBeenCalledTimes(1);
-    expect(mockTodoService.deleteTodo).toHaveBeenCalledTimes(0);
+    expect(mockTodoService.deleteTodo).toHaveBeenCalledWith(
+      dto.id,
+      mockUser.id,
+    );
   });
 });
